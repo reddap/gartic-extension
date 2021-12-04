d594fe108af6609e489eac0e32e83d5fa03fc4e2
reddap <70059776+reddap@users.noreply.github.com>
2021-12-04 18:02:11 -0300

    Update script.js

--- auto-draw/script.js
+++ auto-draw/script.js
@@ -1,5 +1,6 @@
 // get mouse coords https://stackoverflow.com/a/41067865
 // update: agora pega o nome dos eventos junto às coordenadas.
+
 var c, mouseDown = !1,
 	eventName = [],
 	cordX = [],
@@ -19,24 +20,22 @@ window.addEventListener("mousedown", function(e) {
 
 
 
+//update: adaptado à nova ordem de coordenadas
 
-// fix em falta
-async function d(cordinha) {
-	var t = document.querySelector("canvas"),
-		n = t.getBoundingClientRect();
-
-	function eventos(e, x, y) {
-		return new MouseEvent(e, {
-			bubbles: !0,
-			clientX: x * n.width / 1020 + n.x,
-			clientY: y * n.height / 606 + n.y,
-			button: 0
-		})
-	}
-	t.dispatchEvent(eventos("mousedown", cordinha.x[0], cordinha.y[0]));
-	for (var i = 0; i < cordinha.x.length;) t.dispatchEvent(eventos("mousemove", cordinha.x[i], cordinha.y[i])), i++, await new Promise(wait => setTimeout(wait, 50));
-	t.dispatchEvent(eventos("mouseup", cordinha.x[cordinha.x.length - 1], cordinha.y[cordinha.y.length - 1]))
+function eventos(e, x, y) {
+	return new MouseEvent(e, {
+		bubbles: !0,
+		clientX: x * t.width / 1020 + t.x,
+		clientY: y * t.height / 606 + t.y,
+		button: 0
+	})
 }
+for (var canvas = document.querySelector("canvas"),
+     t = canvas.getBoundingClientRect(),
+     i = 0; i < c.e.length;)
+	canvas.dispatchEvent(eventos(c.e[i],
+	c.x[i], c.y[i])), i++;
+
 
 
 /*
