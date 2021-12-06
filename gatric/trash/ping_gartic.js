350cb3b38df173903e7b38b98c65c4f9e211e811
reddap <70059776+reddap@users.noreply.github.com>
2021-12-06 19:44:46 -0300

    Update ping_gartic.js

--- gatric/trash/ping_gartic.js
+++ gatric/trash/ping_gartic.js
@@ -1,17 +1,12 @@
-'use strict';
+"use strict";
 async function ping() {
-  let start = Date.now();
-  let promise = await fetch("https://gartic.com.br/");
-  let rightValue = Date.now();
-  return `${rightValue - start}ms`;
-  ping().then(async(animate_param) => {
-    console.log(animate_param);
-  });
+	let t = Date.now();
+	await fetch("https://gartic.com.br/");
+	return `${Date.now()-t}ms`
 }
 setInterval(() => {
-  ping().then((b) => {
-    if (document.location.href.includes("https://gartic.com.br/0") == true) {
-      document.querySelectorAll("input")[8].attributes[0].textContent = b;
-    }
-  });
-}, 3000);
+	ping().then(t => {
+		1 == document.location.href.startsWith("https://gartic.com.br/0") &&
+      (document.querySelectorAll("input")[8].attributes[0].textContent = t)
+	})
+}, 3e3);
