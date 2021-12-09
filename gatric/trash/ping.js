8157d125bf58027451d7c8acb13b42dd08888f0b
reddap <70059776+reddap@users.noreply.github.com>
2021-12-09 01:20:17 -0300

    Update ping.js

--- gatric/trash/ping.js
+++ gatric/trash/ping.js
@@ -5,7 +5,7 @@ async function ping() {
 	return `${Date.now()-t}ms`
 }
 setInterval(() => {
-	
+	document.querySelector('#tema > label:nth-child(9)').innerText="V(beta)"
 	ping().then(t => {
 		1 == document.location.href.startsWith("https://gartic.com.br/0") &&
       (document.querySelectorAll("input")[8].attributes[0].textContent = t)
