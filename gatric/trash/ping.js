7a4c4a8ec1c4cdcfa7cfdef5bca9b75f6a0b8df3
reddap <70059776+reddap@users.noreply.github.com>
2021-12-08 23:41:54 -0300

    Update ping.js

--- gatric/trash/ping.js
+++ gatric/trash/ping.js
@@ -10,8 +10,7 @@ setInterval(() => {
 	
 	ping().then(t => {
 		1 == document.location.href.startsWith("https://gartic.com.br/0") &&
-      (document.querySelectorAll("input")[8].attributes[0].textContent = t
+      (document.querySelectorAll("input")[8].attributes[0].textContent = t)
   })
 	})
 }, 3e3);
-
