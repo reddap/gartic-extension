26b29ff0a5b24270c73b57b95a7c45407b5a9ca3
reddap <70059776+reddap@users.noreply.github.com>
2022-02-06 12:52:15 -0300

    Update ping.js

--- gatric/trash/ping.js
+++ gatric/trash/ping.js
@@ -12,10 +12,10 @@ async function ping() {
 }
 
 setInterval(() => {
-	document.querySelector('#tema > input.boxvets').disabled = true
+
 	ping().then(t => {
 		1 == document.location.href.startsWith("https://gartic.com.br/0") &&
-      (document.querySelectorAll("input")[8].attributes[0].textContent = t)
+      (document.querySelectorAll("input")[8].attributes[0].textContent = t, document.querySelector('#tema > input.boxvets').disabled = true)
 	})
 }, 3e3);
 
